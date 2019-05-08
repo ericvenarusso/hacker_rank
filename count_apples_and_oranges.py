943408e528d8ab3d7d9c6e64c16b057f46e4f9e7
Eric Buzato Venarusso <36274852+ericvenarusso@users.noreply.github.com>
2019-05-08 14:18:23 -0300

    Update count_apples_and_oranges.py

--- count_apples_and_oranges.py
+++ count_apples_and_oranges.py
@@ -6,21 +6,18 @@ Link: https://www.hackerrank.com/challenges/apple-and-orange/problem
 #!/usr/bin/python
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
-    house_region = list(range(s, t + 1, 1))
-    apples_near_house = 0
-    oranges_near_house = 0
-    fruits_near_house = []
-    
-    for i_apple in apples:
-        if (a + i_apple) in house_region:
-            apples_near_house+=1
+
+    def _count_near_fruits(tree_distance, fruits, fruits_near_house = 0):
+        for i_fruit in fruits:
+            fruit_distance = (tree_distance + i_fruit)
             
-    print(apples_near_house)
-    
-    for i_orange in oranges:
-        if (b + i_orange) in house_region:
-            oranges_near_house+=1
+            if (fruit_distance >= s) and (fruit_distance  <= t):
+                fruits_near_house+=1
+                
+        return fruits_near_house
             
-    print(oranges_near_house)
+ 
+    print(_count_near_fruits(a, apples))
+    print(_count_near_fruits(b, oranges))
     
 countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
