f557365fa115673f4dc354e3bcd80a07fb966a52
Eric Venarusso <eric.venarusso-ext@viavarejo.com.br>
2019-05-08 12:02:20 -0300

    first commit

---
+++ count_apples_and_oranges.py
@@ -0,0 +1,19 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    house_region = list(range(s, t + 1, 1))
+    apples_near_house = 0
+    oranges_near_house = 0
+    fruits_near_house = []
+    
+    for i_apple in apples:
+        if (a + i_apple) in house_region:
+            apples_near_house+=1
+            
+    print(apples_near_house)
+    
+    for i_orange in oranges:
+        if (b + i_orange) in house_region:
+            oranges_near_house+=1
+            
+    print(oranges_near_house)
+    
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
---
+++ grading_students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def gradingStudents(grades):
+    new_grades = []
+    for i in grades:
+        if i < 38:
+            new_grades.append(i)
+        else:
+            multiples_of_5 = ((i // 5) * 5) + 5
+            if (multiples_of_5 - i) < 3:
+                new_grades.append(multiples_of_5)
+            else:
+                new_grades.append(i)
+                
+    return new_grades
+
+gradingStudents([73, 67, 38, 33])
